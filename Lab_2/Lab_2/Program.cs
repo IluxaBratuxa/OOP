4e86602bed2ab7aa2dfa14d9558f0cb232ceb6c7
Andrej <andrey.pisaryk@gmail.com>
2020-10-06 00:29:09 +0300

    Labs 2-4

---
+++ Lab_2/Lab_2/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - variables & types
+            //Types C#
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 1////////////////\n");
+            Console.ResetColor();
+
+            //Console.WriteLine();
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("//////////////////////////////////////////////////");
+            //Console.ResetColor();
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("//////////////////////Task 1//////////////////////");
+            //Console.ResetColor();
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("//////////////////////////////////////////////////");
+            //Console.ResetColor();
+            //Console.WriteLine();
+
+
+            int Int = 25;
+            uint UInt = 2510;
+            short Short = 32138;
+            ushort UShort = 62319;
+            long Long = 20345227281887;
+            ulong ULong = 52528602034522;
+            float Float = 25.10f;
+            bool Boolean = true;
+            char Char = 'C';
+            double Double = 25.10;
+            string Str = "Andrej";
+            byte Byte = 10;
+            sbyte SByte = -100;
+            Console.WriteLine("My name is " + Str);
+            Console.Write("Enter the number: ");
+            Int = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            /// Преобразование типов & Convert
+            Float = Int;
+            Int = Short;
+            Double = Float;
+            Long = UInt;
+            UInt = UShort;
+
+            Int = (int)Float;
+            Short = (short)Char;
+            Char = (char)Int;
+            Float = (float)Double;
+            Byte = (byte)Int;
+            Console.WriteLine("Явное преобразование int в byte: " + Byte);
+            Int = Convert.ToInt32(Boolean);
+            Console.WriteLine("Convert Boolean to int :" + Int);
+            Console.WriteLine();
+
+            //Boxing & Unboxing
+            int bi = 2510;
+            object box = bi;
+            int unb = (int)box;
+            Console.WriteLine("Unboxing: " + unb);
+            Console.WriteLine();
+
+            //Var
+            var anonVar = 10.25;
+            double d = Double + anonVar;
+            Console.WriteLine("Var: " + d);
+            Console.WriteLine();
+
+            //Nullable
+            int? nullVar = null;
+            // nullVar = Int;
+            if (nullVar.HasValue)
+                Console.WriteLine("nullVar is equal " + nullVar.Value);
+            else
+                Console.WriteLine("nullVar is empty");
+            Console.WriteLine();
+
+            // Var error
+            var v = 10;
+            //v = 10.25; - тип переменной var сохроняется до конца ее существования
+
+
+            //Task 2 - strings
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 2////////////////\n");
+            Console.ResetColor();
+
+            //Compare
+            string name = "Andrej";
+            string surname = "Pisarik";
+            if (String.Compare(name, surname) == 0)
+                Console.WriteLine("Strings are equal");
+            else
+                Console.WriteLine("Strings aren't equal");
+            Console.WriteLine();
+
+            // Operations with String 
+            string str1 = "Evening in the house.";
+            string str2 = "Long live Belarus. ";
+            string str3 = "We belive, we can, we will win. ";
+            Console.WriteLine("Concat: " + String.Concat(str3, str2));
+            char[] copyStr = new char[25];
+            str1.CopyTo(0, copyStr, 0, 7);
+            Console.WriteLine(copyStr);
+            Console.WriteLine("Substring: " + str2.Substring(5, 4));
+            string[] words = str1.Split(new char[] { ' ' });
+            Console.WriteLine("Split: ");
+            for (int j = 0; j < words.Length; j++)
+                Console.WriteLine(words[j]);
+            Console.WriteLine("Insert: " + str1.Insert(8, str2));
+            str1 = str1.Remove(0, 7);
+            Console.WriteLine($"Remove: {str1}");
+            Console.WriteLine();
+
+            //isNullOrEmpty
+            string strTemp = String.Empty;
+            string nullString = null;
+            if (String.IsNullOrEmpty(strTemp))
+            {
+                strTemp = "Liberty";
+                Console.WriteLine(strTemp);
+            }
+            else
+                Console.WriteLine("String isn't empty");
+            Console.WriteLine();
+
+            //StringBuilder
+            StringBuilder strBuild = new StringBuilder("Hello world");
+            Console.WriteLine("Build string: " + strBuild);
+            Console.WriteLine("Length: " + strBuild.Length);
+            Console.WriteLine("Capacity: " + strBuild.Capacity);
+            strBuild = strBuild.Remove(5, 1);
+            Console.WriteLine("Remove some index: " + strBuild);
+            strBuild = strBuild.Insert(0, "*");
+            strBuild = strBuild.Append("*");
+            Console.WriteLine("Add some symbol: " + strBuild);
+
+
+            //Task 3 - arrays
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 3////////////////\n");
+            Console.ResetColor();
+
+            int[,] arr = { { 10, 2, 3 }, { 25, 10, 384 } };
+            for (var k = 0; k < 2; k++)
+            {
+                for (var t = 0; t < 3; t++)
+                {
+                    Console.Write("\t" + arr[k, t]);
+                }
+                Console.WriteLine("\n\n");
+            }
+
+            string[] arrString = { "Hello", "World", "Freedom", "London", "Belarus" };
+            int arrSize = arrString.Length;
+            int index;
+            Console.WriteLine("Array size: " + arrSize);
+            foreach (var r in arrString)
+                Console.Write("  " + r);
+            Console.WriteLine();
+            do
+            {
+                Console.WriteLine("Enter the index of the element which you want to replace: ");
+                index = Convert.ToInt32(Console.ReadLine());
+                if (index >= arrSize)
+                    Console.WriteLine("Entered index is more than the array size. Try again, please");
+            } while (index >= arrSize);
+
+            Console.WriteLine("Enter a new string: ");
+            arrString[index - 1] = Convert.ToString(Console.ReadLine());
+            foreach (var r in arrString)
+                Console.Write("  " + r);
+            Console.WriteLine();
+
+            double[][] arrJag = { new double[2], new double[3], new double[4] };
+            Console.WriteLine("Enter the array elements: ");
+            for (var j = 0; j < arrJag.Length; j++)
+            {
+                for (var k = 0; k < arrJag[j].Length; k++)
+                {
+                    arrJag[j][k] = Convert.ToDouble(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+            for (var j = 0; j < arrJag.Length; j++)
+            {
+                for (var k = 0; k < arrJag[j].Length; k++)
+                {
+                    Console.Write("\t" + arrJag[j][k]);
+                }
+                Console.WriteLine();
+            }
+
+            var arrVar = new[] { 10, 23, 135 };
+            var varString = "Hello world";
+
+            //Task 4 - tuples
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 4////////////////\n");
+            Console.ResetColor();
+
+            (string name, string surname, int age, char word, ulong number) person = ("Elon", "Musk", 49, 'X', 729439101451);
+            Console.WriteLine($"About founder of SpaceX: \n Name: {person.name}\n Surname: {person.surname}\n Age:{person.age}\n Some word: {person.word}\n Number: {person.number}");
+            Console.WriteLine($"Elemets:\n First: {person.Item1} \n Third: {person.Item3} \n Fourth: {person.Item4}");
+
+            var tuple1 = ("BSTU", 19);
+            (string university, int age) = tuple1;
+
+            var tuple2 = ("Kamenets", 1276);
+            var (city, date) = tuple2;
+
+            var BSTU = String.Empty;
+            var varAge = 0;
+            (BSTU, varAge) = tuple1;
+
+            var tuple3 = ("Hello", "World", 1276, true);
+            string[] arr3 = new string[3];
+            (arr3[0], arr3[1], _, _) = tuple3;
+            Console.WriteLine($"arr[0] = {arr3[0]}\narr[1] = {arr3[1]}");
+
+            if (tuple1 == tuple2)
+                Console.WriteLine("Tuples are equal");
+            else
+                Console.WriteLine("Tuples aren't equal");
+
+            //Task 5 - local function 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 5////////////////\n");
+            Console.ResetColor();
+
+            string localString = "Hello, bro";
+            int[] localArray = { 10, 20, 35, 1, 25, 0, 8 };
+
+            Console.WriteLine($"String: {localString}");
+            Console.Write("Array: ");
+            foreach (var n in localArray)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine("\n");
+
+            (int min, int max, int sum, char firstWord) TestFunction(string tempStr,int[] tempArr)
+            {
+                int min = tempArr[0];
+                int max = tempArr[0];
+                int sum = 0;
+                char[] allWords = tempStr.ToArray();
+                char firstWord = allWords[0];
+
+                for (var i = 0; i < tempArr.Length; i++)
+                {
+                    if (tempArr[i] > max)
+                        max = tempArr[i];
+                    else if (tempArr[i] < min)
+                        min = tempArr[i];
+
+                    sum = sum + tempArr[i];
+                }
+
+                return (min, max, sum, firstWord);
+            }
+
+            (int min, int max, int sum, char firstWord) result = TestFunction(localString, localArray);
+            Console.WriteLine($"Elements from function:\n Min: {result.min}\n Max: {result.max}\n Sum: {result.sum}\n First word: {result.firstWord}");
+
+            //Task 6 - checked & unchecked 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n/////////////////Task 6////////////////\n");
+            Console.ResetColor();
+
+            int Checked()
+            {
+                int a = checked(2_147_483_647); // 2_147_483_647 + 10 - Error
+                return a;
+            }
+
+            int Unchecked()
+            {
+                int b = unchecked(2_147_483_647 + 10);
+                return b;
+            }
+
+            int checkedVar = Checked();
+            int uncheckedVar = Unchecked();
+            Console.WriteLine($"Result of checked: {checkedVar}\nResult of unchecked: {uncheckedVar}");
+            Console.Read();
+
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("/////////////////////////////////////////////Primite labu, plz))//////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Read();
+        }
+
+
+    }
+}
---
+++ Lab_3/Program.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    public partial class Work // partial class - является частичным классом, partial показывает, что класс может быть разбит на несколько классов
+    {
+        public void Student()
+        {
+            Console.WriteLine("The work perfomed by Andrjushka)");
+        }
+    }
+    public partial class Work
+    {
+        public void Teacher()
+        {
+            Console.WriteLine("The work accepted by Alex Victorovich)\n");
+        }
+    }
+
+    class Bus
+    {
+        public string driverFullName { get; set; }
+        private int busNumber;
+        public int BusNumber
+        {
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Bus number can't be less than 0");
+                else
+                    busNumber = value;
+            }
+            get
+            {
+                return busNumber;
+            }
+        }
+        private int numberOfPath;
+        public int NumberOfPath
+        {
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Path number can't be less than 0");
+                else
+                    numberOfPath = value;
+            }
+            get
+            {
+                return numberOfPath;
+            }
+        }
+        public string busBrand { get; set; }
+        public int yearOfExp;
+        public int YearOfExp
+        {
+            set
+            {
+                if (value > 1980 && value < 2021)
+                    yearOfExp = value;
+                else
+                    Console.WriteLine("Bus can't be more than 40 years old");
+            }
+
+        }
+        public readonly double ID;
+        public const int constVar = 2;
+        private static int counter = 0;
+
+        public Bus() // Конструктор без параметров (по-умолчанию)
+        {
+            driverFullName = "No information";
+            busNumber = 100;
+            NumberOfPath = 100;
+            busBrand = "No information";
+            yearOfExp = 2000;
+            ID = (busNumber*NumberOfPath)/ constVar;
+            counter++;
+        }
+       
+        public Bus(string fullName, int number, int path, string brand, int year) // Конструктор с параметрами
+        {
+           driverFullName = fullName;
+           busNumber = number;
+           NumberOfPath = path;
+           busBrand = brand;
+           yearOfExp = year;
+           ID = (busNumber * NumberOfPath) / constVar;
+           counter++;
+        }
+
+        public Bus(string fullName)
+        {
+            driverFullName = fullName;
+            busNumber = 26;
+            NumberOfPath = 26;
+            busBrand = "MAZ";
+            yearOfExp = 1994;
+            ID = (busNumber * NumberOfPath) / constVar;
+            counter++;
+        }
+
+        public void BusAge(ref int yearOfExp, out int busAge) // Метод для рассчета возраста автобуса
+        {
+            busAge = DateTime.Now.Year - yearOfExp;
+            
+        }
+
+        static Bus() // Статический метод, который запускается при создании объекта класса Bus
+        {
+            Console.WriteLine("Welcome to the bus station!\n");
+        }
+
+        //private Bus() { } - закрытый конструктор. В данном случае не нужно использовать, т.к. он запрещает создания экземпляров класса по-умолчанию
+
+        public static void CounterOfBuses(out int amountBuses) // Метод, который считает количество созданных объектов класса
+        {
+            amountBuses = counter;
+            Console.WriteLine($"Amount of buses: {counter} \n");
+        }
+
+        public void Info() // Метод, который выводит информацию об объекте
+        {
+            Console.WriteLine("Information about bus:");
+            Console.WriteLine($"Driver name: {driverFullName}\n" +
+                              $"Number: {busNumber}\n" +
+                              $"Number of the path: {numberOfPath}\n" +
+                              $"Brand: {busBrand}\n" +
+                              $"Year of exploitation: {yearOfExp}\n" +
+                              $"ID: {ID}\n");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Work lab = new Work();
+            lab.Student();
+            lab.Teacher();
+
+            var anonObj = new { name = "John S", age = 40, hometown = "Minsk", number = 375298543709 };
+            Console.WriteLine($"Анонимный объект:\n" +
+                              $"Имя: {anonObj.name}\n" +
+                              $"Возраст: {anonObj.age}\n" +
+                              $"Город: {anonObj.hometown}\n" +
+                              $"Номер: {anonObj.number}\n");
+            Bus[] busArray = new Bus[5];
+            busArray[0] = new Bus();
+            busArray[1] = new Bus("Musk I", 16, 25, "Tesla", 2019);
+            busArray[2] = new Bus("Gates B", 90, 10, "Windows", 1984);
+            busArray[3] = new Bus("Shiman D", 10, 25, "FIT", 2015);
+            busArray[4] = new Bus("BigLu A");
+
+            int amountBuses;
+            Bus.CounterOfBuses(out amountBuses);
+
+            int choice;
+            do
+            {
+                Console.WriteLine("Выберите действие: ");
+                Console.WriteLine("0 - Выход");
+                Console.WriteLine("1 - Вывести информацию об автобусе");
+                Console.WriteLine("2 - Вывести возраст автобуса");
+                Console.WriteLine("3 - Вывести список автобусов по заданному маршруту");
+                Console.WriteLine("4 - Вывести список автобусов которые эксплуатируются больше заданного срока");
+                Console.WriteLine("5 - Сравнить данные автобусов");
+                Console.WriteLine("Ваш выбор: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        {
+                            //Console.WriteLine("Goodbye :)");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("Введите порядковый номер автобуса (1-5): ");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            if (number > 0 && number <= amountBuses)
+                            {
+                                busArray[number-1].Info();
+                            }
+                            else
+                                Console.WriteLine("Такого автобуса нет");
+
+                            break;
+                        }
+                    case 2:
+                        {
+                           Console.WriteLine("Введите порядковый номер автобуса (1-5): ");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            int busAge;
+                            if (number > 0 && number <= amountBuses)
+                            {
+                                busArray[number-1].BusAge(ref busArray[number-1].yearOfExp, out busAge);
+                                Console.WriteLine($"Возраст автобуса = {busAge}\n");
+                            }
+                            else
+                                Console.WriteLine("Такого автобуса нет");
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите номер маршрута, чтобы узнать, какие автобусы по нему ходят: ");
+                            int path = Convert.ToInt32(Console.ReadLine());
+                            int counter = 0;
+                            Console.WriteLine("Автобусы по маршруту №" + path+":\n");
+                            for(int i=0;i<busArray.Length;i++)
+                            {
+                                if (busArray[i].NumberOfPath == path)
+                                {
+                                    busArray[i].Info();
+                                    counter++;
+                                }
+                            }
+                            if (counter <= 0)
+                                Console.WriteLine("Ничего не найдено");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Введите срок эксплуатации автобуса: ");
+                            int exploitation = Convert.ToInt32(Console.ReadLine());
+                            int busAge;
+                            if (exploitation<0)
+                            {
+                                Console.WriteLine("Срок эксплуатации не может быть меньше нуля");
+                                break;
+                            }
+                            Console.WriteLine("Список автобусов со сроком эксплуатации больше заданного: ");
+                            for(int i=0;i < busArray.Length; i++)
+                            {
+                                busArray[i].BusAge(ref busArray[i].yearOfExp, out busAge);
+                                if (busAge>exploitation)
+                                {
+                                    busArray[i].Info();
+                                }
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Введите порядковый номер первого автобуса (1-5): ");
+                            int number1 = Convert.ToInt32(Console.ReadLine());
+                            if(number1<=0 && number1>5)
+                            {
+                                Console.WriteLine("Такого автобуса нет");
+                                break;
+                            }
+                            Console.WriteLine("Введите порядковый номер второго автобуса (1-5): ");
+                            int number2 = Convert.ToInt32(Console.ReadLine());
+                            if (number2 <= 0 && number2 > 5)
+                            {
+                                Console.WriteLine("Такого автобуса нет");
+                                break;
+                            }
+                            Console.WriteLine("Первый автобус: ");
+                            busArray[number1-1].Info();
+                            Console.WriteLine("Второй автобус: ");
+                            busArray[number2-1].Info();
+
+                            bool compare = busArray[number1-1].Equals(busArray[number2-1]);
+                            if (compare)
+                                Console.WriteLine("Данные автобусов одинаковые");
+                            else
+                                Console.WriteLine("Данные автобусов не совпадают");
+                            Console.WriteLine($"Hash код первого автобуса: {busArray[number1-1].GetHashCode()}\n" +
+                                              $"Hash код второго автобуса: {busArray[number2-1].GetHashCode()}\n" +
+                                              $"Тип данных первого автобуса: {busArray[number1-1].GetType()}\n" +
+                                              $"Тип данных второго автобуса: {busArray[number2 - 1].GetType()}\n" +
+                                              $"Имя водителя первого автобуса: {busArray[number1 - 1].driverFullName.ToString()}\n" +
+                                              $"Имя водителя второго автобуса: {busArray[number2 - 1].driverFullName.ToString()}\n");
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Такого пункта в меню нет. Попробуйте еще раз");
+                            break;
+                        }
+           
+                }
+
+            } while (choice!=0);
+
+
+
+
+            
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("/////////////////////////////////////////////Primite labu, plz))//////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
